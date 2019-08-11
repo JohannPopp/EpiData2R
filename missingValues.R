6dbd448848fe22cfcf364f2f57dc6814f545071e
JohannPopp <johannpopp@gmx.net>
2019-08-11 14:58:25 +0200

    More elegant conversion of missing value to NA

--- missingValues.R
+++ missingValues.R
@@ -17,38 +17,25 @@ epx.missing <- function(dat, info){
   indexMissing <- lapply(info$valLabels, function(x) grep("missing=\"true\"", x))
   # Identify missing value codes
   missingCodes <- as.list(mapply(function(x, y) x[y], valueCodes, indexMissing))
-  
-  
+
   # Index the corresponding value label set for each of the variables
   indexValLabSet <- lapply(info$fieldValLabSets, function(x) which(xml2::xml_attr(info$valLabelSets, "id") == x))
   # Missings per variable
-  missingsPerVar <- missingCodes[as.numeric(paste(indexValLabSet))]
+  missingsPerVar <- lapply(missingCodes[as.numeric(paste(indexValLabSet))],
+                           paste, collapse = "|")
+  missingsPerVar[missingsPerVar == ""] <- NA
   
   # Convert defined missing values into NA
-  
-  # Add a first row containting defined missing codes for each variable 
-  # and a second row, indicating which variable contains defined missings.
-  dat2 <- rbind(lapply(missingsPerVar, function(x) {paste(x, collapse = "|")}),
-                lapply(missingsPerVar, length) > 0,
-                dat)
-  
-  # Set defined missings to NA for those variables that contain defined missings
-  dat3 <- as.data.frame(apply(dat2[,dat2[2,] == TRUE], 2, function(x){
-    x[grepl(x[1], x)] <- NA
-    x
-  }), stringsAsFactors = FALSE)
-  
-  # Enter variables with defined missings back into the data frame
-  dat2[,dat2[2,] == TRUE] <- dat3
-  
-  # Delete rows 1 and 2 (that contain additional information about defined missings)
-  dat2 <- dat2[-c(1,2),]
-  # Reset rownames
-  rownames(dat2) <- NULL
-  
+  dat[
+    mapply(function(x, y){
+      grepl(y, x)
+    },
+    x = dat, 
+    y = missingsPerVar) == 1
+    ] <- NA
+
   # Convert undefined missing values in text fields (expressed as "NA") into NA
-  dat2[dat2 == "NA"] <- NA
-  
-  
-  dat2
+  dat[dat == "NA"] <- NA
+ 
+  dat
 }
